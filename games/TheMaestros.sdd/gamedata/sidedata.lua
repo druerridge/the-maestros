8f9327459a71c9ad5bc99236eddbb9ef787a7e37
Dru Erridge <druerridge@gmail.com>
2013-01-25 20:08:58 -0800

    creating skeletal folder structure of TheMaestros game

---
+++ games/TheMaestros.sdd/gamedata/sidedata.lua
@@ -0,0 +1,7 @@
+local sidedata = {
+	{
+	name = "TANKS",
+	startUnit = "shitty_tank",
+	}
+}
+return sidedata
---
+++ games/TheMaestros.sdd/modinfo.lua
@@ -0,0 +1,17 @@
+local modinfo = {
+	name = "Empty Mod plus Tank",
+	shortname = "Tank",
+	game = "Empty Example Mod plus Tank Unit",
+	shortgame = "Tank",
+	description = "Mod with the bare minimum to run, plus a basic tank unit. Based on FLOZi's MDK",
+	url = "http://www.somewebsite.com/",
+	
+	modtype = "1",
+
+	depend = {
+		"cursors.sdz",
+		"springcontent.sdz",
+	}
+}
+
+return modinfo
