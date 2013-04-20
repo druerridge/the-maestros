2dc7ca945ee0b9052ab4420213218b8e7c0d42f4
Sean Saleh <seansaleh>
2013-04-20 16:57:06 -0700

    Added test_gadget that prints 1 / sec
    also removed NPP backup files!?

---
+++ games/TheMaestros.sdd/LuaRules/Gadgets/Test-Gadget.lua
@@ -0,0 +1,26 @@
+function gadget:GetInfo()
+        return {
+                name = "testGadget",
+                desc = " Prints message every frame",
+                author = "Sean", 
+                date = "2013-03-20",
+                license = "Public Domain",
+                layer = 19,
+                enabled = true
+        }
+end
+
+if (gadgetHandler:IsSyncedCode()) then
+
+--SYNCED
+
+function gadget:GameFrame(f)
+        if f%30 < .1 then
+            Spring.Echo("test")
+                
+        end
+end
+
+else
+--UNSYNCED
+end
