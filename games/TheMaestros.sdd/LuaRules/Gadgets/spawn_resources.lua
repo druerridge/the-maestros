861bd85dcd19097ae522ef3cc7aa813b5c4accd8
Dru Erridge <druerridge@gmail.com>
2013-04-25 14:06:30 -0700

    resource spawning now works with arkanium feature locations instead

--- games/TheMaestros.sdd/LuaRules/Gadgets/spawn_resources.lua
+++ games/TheMaestros.sdd/LuaRules/Gadgets/spawn_resources.lua
@@ -19,39 +19,28 @@ local first = true
 local radius = 100
 
 function gadget:GameFrame(f)
-    --Spawns arkanium every once in a while
-    if (first) then
-        Spring.Echo("in frist if")
-        local allUnits = Spring.GetAllUnits()
-        for i = 1, #allUnits do
-            local u = allUnits[i]
-            local ud = Spring.GetUnitDefID(u)
-            -- id 1 == arkaniumspawner
-            if ud == 1 then
-                local x, y, z = Spring.GetUnitPosition(u)
-                spawnLocations[#spawnLocations + 1] = {x, y, z}
-
-                Spring.DestroyUnit(u, false, true)
-            end
-        end
-        -- make sure this only runs the first time
-        first = false
-    end
-
-    -- 30 frames / second, 900 = 30 seconds
+    
     if f%900 < .1 then
-        Spring.Echo("In second if")
-        Spring.Echo(#spawnLocations)
         for j = 1, #spawnLocations do
             local x = spawnLocations[j][1]
             local y = spawnLocations[j][2]
             local z = spawnLocations[j][3]
-            Spring.CreateFeature("arkanium", x+radius, Spring.GetGroundHeight(x,z)+5, z)
-            Spring.CreateFeature("arkanium", x-radius, Spring.GetGroundHeight(x,z)+5, z)
-            Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z+radius)
-            Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z-radius)
+            Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z)
         end
+    end
 
+end
+
+function gadget:Initialize()
+    local allFeatures = Spring.GetAllFeatures()
+    for i = 1, #allFeatures do
+        local ff = allFeatures[i]
+        local fd = Spring.GetFeatureDefID(ff)
+        -- arkanium
+        if fd == FeatureDefNames["arkanium"].id then
+            local x, y, z = Spring.GetFeaturePosition(ff)
+            spawnLocations[#spawnLocations + 1] = {x, y, z}
+        end
     end
 end
 
