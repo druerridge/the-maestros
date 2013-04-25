2b4dff7082086520b1e4c892e53554c4785c211d
Dru Erridge <druerridge@gmail.com>
2013-04-25 12:36:53 -0700

    fixing arkanium spawning to be more efficient and removing the annoying arkanium spawner

--- games/TheMaestros.sdd/LuaRules/Gadgets/spawn_resources.lua
+++ games/TheMaestros.sdd/LuaRules/Gadgets/spawn_resources.lua
@@ -1,7 +1,7 @@
 function gadget:GetInfo()
         return {
                 name = "spawnResources",
-                desc = " Spawns resources every turn",
+                desc = " Spawns resources every so often",
                 author = "Sean", 
                 date = "2013-03-20",
                 license = "Public Domain",
@@ -14,36 +14,49 @@ if (gadgetHandler:IsSyncedCode()) then
 
 --SYNCED
 
+local spawnLocations = {}
+local first = true
+local radius = 100
+
 function gadget:GameFrame(f)
-        --Spawns arkanium every once in a while
-        if f%1000 < .1 then
-            local allUnits = Spring.GetAllUnits()
-            for i = 1, #allUnits do
-                local u = allUnits[i]
-                local ud = Spring.GetUnitDefID(u)
-                local team = Spring.GetUnitTeam(u)
-                -- id 1 == arkaniumspawner
-                if ud == 1 then
-                    Spring.Echo("I'm an arkaniumspawner")
-                    local x, y, z = Spring.GetUnitPosition(u)
-                    local radius = 100
-                    Spring.CreateFeature("arkanium", x+radius, Spring.GetGroundHeight(x,z)+5, z)
-                    Spring.CreateFeature("arkanium", x-radius, Spring.GetGroundHeight(x,z)+5, z)
-                    Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z+radius)
-                    Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z-radius)
-                end
+    --Spawns arkanium every once in a while
+    if (first) then
+        Spring.Echo("in frist if")
+        local allUnits = Spring.GetAllUnits()
+        for i = 1, #allUnits do
+            local u = allUnits[i]
+            local ud = Spring.GetUnitDefID(u)
+            -- id 1 == arkaniumspawner
+            if ud == 1 then
+                local x, y, z = Spring.GetUnitPosition(u)
+                spawnLocations[#spawnLocations + 1] = {x, y, z}
+
+                Spring.DestroyUnit(u, false, true)
             end
         end
+        -- make sure this only runs the first time
+        first = false
+    end
 
-    --[[
-        if f%30 < .1 then
-			Xra=math.random(0,4000)
-			Zra = math.random(0,4000)
-            local flagID = Spring.CreateFeature("arkanium", Xra, Spring.GetGroundHeight(Xra,Zra)+5, Zra)
+    -- 30 frames / second, 900 = 30 seconds
+    if f%900 < .1 then
+        Spring.Echo("In second if")
+        Spring.Echo(#spawnLocations)
+        for j = 1, #spawnLocations do
+            local x = spawnLocations[j][1]
+            local y = spawnLocations[j][2]
+            local z = spawnLocations[j][3]
+            Spring.CreateFeature("arkanium", x+radius, Spring.GetGroundHeight(x,z)+5, z)
+            Spring.CreateFeature("arkanium", x-radius, Spring.GetGroundHeight(x,z)+5, z)
+            Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z+radius)
+            Spring.CreateFeature("arkanium", x, Spring.GetGroundHeight(x,z)+5, z-radius)
         end
-    ]]
+
+    end
 end
 
 else
+
 --UNSYNCED
+
 end
